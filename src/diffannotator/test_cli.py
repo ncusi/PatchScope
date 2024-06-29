a723a150c24c772b34dd4f6ec5f966249f3545ca
Jakub Narębski <jnareb@mat.umk.pl>
2024-06-29 20:49:34 +0200

    Add a basic test for --purpose-to-annotation
    It just check that there are no errors, and that there was a message
    about changed mapping.  There is no check that mapping works, but there
    are other tests covering this functionality in API tests in
    test_annotate.py (rather that test_cli.py).

--- src/diffannotator/test_cli.py
+++ src/diffannotator/test_cli.py
@@ -65,3 +65,24 @@ def test_annotate_patch_with_line_callback(tmp_path: Path):
 
     assert result.exit_code == 0, \
         "app runs 'patch' subcommand with line callback from file defining function without errors"
+
+
+def test_annotate_patch_with_purpose_to_annotation(tmp_path: Path):
+    file_path = Path('test_dataset/tqdm-1/c0dcf39b046d1b4ff6de14ac99ad9a1b10487512.diff')
+    save_path = tmp_path.joinpath(file_path).with_suffix('.json')
+
+    # callback as string
+    result = runner.invoke(app, [
+        "--purpose-to-annotation=",  # reset mapping
+        "--purpose-to-annotation=docs:documentation",  # explicit mapping
+        "--purpose-to-annotation=test",  # implicit mapping
+        "patch", f"{file_path}", f"{save_path}"
+    ])
+
+    assert result.exit_code == 0, \
+        "app runs 'patch' subcommand with a --purpose-to-annotation without errors"
+    assert \
+        "documentation\t=>\tdocumentation" not in result.stdout and \
+        "docs\t=>\tdocumentation" in result.stdout and \
+        "test\t=>\ttest" in result.stdout, \
+        "app correctly prints that mapping changed to the requested values"
