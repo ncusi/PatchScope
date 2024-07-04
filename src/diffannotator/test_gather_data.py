7f0c4610bae946552acec62f4bf8f42820901c35
Mikołaj Fejzer <mfejzer@mat.umk.pl>
2024-07-04 12:29:52 +0200

    test_gather_data.py: Add test_process_data()

--- src/diffannotator/test_gather_data.py
+++ src/diffannotator/test_gather_data.py
@@ -13,3 +13,44 @@ def test_AnnotatedBugDataset():
     assert data._hunk_purposes == Counter({'programming': 5, 'markup': 5, 'other': 2, 'documentation': 1})
     assert data._added_line_purposes == Counter({'programming': 45, 'documentation': 37, 'markup': 13, 'other': 1})
     assert data._removed_line_purposes == Counter({'programming': 25, 'markup': 13})
+
+
+def test_process_data():
+    data = {
+        "synapse/push/mailer.py": {
+            "language": "Python",
+            "type": "programming",
+            "purpose": "programming",
+            "+": [
+                {
+                    "id": 3,
+                    "type": "code",
+                    "purpose": "programming",
+                    "tokens": [
+                        [
+                            51,
+                            [
+                                "Text",
+                                "Whitespace"
+                            ],
+                            "    "
+                        ],
+                        [
+                            55,
+                            [
+                                "Literal",
+                                "String",
+                                "Doc"
+                            ],
+                            "\"\"\"\n"
+                        ]
+                    ]
+                }
+            ]
+        }
+    }
+    result = process_data('e54746bdf7d5c831eabe4dcea76a7626f1de73df.json', data)
+    assert result._processed_files == ['e54746bdf7d5c831eabe4dcea76a7626f1de73df.json']
+    assert result._hunk_purposes == Counter({'programming': 1})
+    assert result._added_line_purposes == Counter({'programming': 1})
+    assert result._removed_line_purposes == Counter()
