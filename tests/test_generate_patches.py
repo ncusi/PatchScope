e711e764fc8ddbf6ab2c798572812229df21cf5a
Jakub Narębski <jnareb@gmail.com>
2024-07-12 00:29:38 +0200

    test_generate_patches.py: Add test for format_patch()
    It tests that the format_patch() method in GitRepo generates patches as
    expected, and that generated patches can be parsed by 'annotate.py's
    annotate_single_diff().

--- tests/test_generate_patches.py
+++ tests/test_generate_patches.py
@@ -2,6 +2,7 @@
 """Test cases for `src/diffannotator/generate_patches.py` module"""
 from pathlib import Path
 
+from diffannotator.annotate import annotate_single_diff
 from diffannotator.generate_patches import GitRepo
 
 
@@ -33,3 +34,50 @@ def test_clone_repository(tmp_path: Path):
         "repo uses absolute path to the repository"
     assert repo.repo.name == 'hellogitworld', \
         "the repo was re-cloned into 'hellogitworld' subdirectory"
+
+
+def test_format_patch(tmp_path: Path):
+    """Test format_patch() method in GitRepo class"""
+    # MAYBE: create fixture
+    test_repo_url = 'https://github.com/githubtraining/hellogitworld.git'
+    repo = GitRepo.clone_repository(
+        repository=test_repo_url,
+        working_dir=tmp_path,
+        make_path_absolute=True,
+    )
+
+    # create directory to save patch(es) to
+    patches_dir = tmp_path / 'patch'
+    patches_dir.mkdir(exist_ok=True)
+    # generate patches (using `git format-patch`)
+    repo.format_patch(output_dir=patches_dir)
+
+    patches_paths = list(patches_dir.glob('*'))
+    assert len(patches_paths) == 1, \
+        "format_patch() created single patch in 'patch' subdirectory"
+
+    # try to parse this patch file
+    file_path = patches_paths[0]
+    patch = annotate_single_diff(file_path)
+    changed_file_name = 'pom.xml'
+    assert len(patch) == 1, \
+        "there is only one file in patch"
+    assert changed_file_name in patch, \
+        f"the '{changed_file_name}' file is found in patch data"
+    assert len(patch[changed_file_name]['-']) == 1, \
+        "there is only one removed line (one changed line)"
+    assert len(patch[changed_file_name]['+']) == 1, \
+        "there is only one added line (one changed line)"
+
+    # create directory to save patch(es) to
+    patches_dir = tmp_path / 'patches'
+    patches_dir.mkdir(exist_ok=True)
+    # generate patches (using `git format-patch`)
+    repo.format_patch(output_dir=patches_dir,
+                      revision_range=['--root', 'HEAD'])  # full history
+
+    patches_paths = list(patches_dir.glob('*'))
+    assert len(patches_paths) == 24, \
+        "format_patch() created patches for all 24 commits in trunk"
+    assert all([path.suffix == '.patch' for path in patches_paths]), \
+        "all created files have '.patch' suffix"
