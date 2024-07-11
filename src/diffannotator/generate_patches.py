b0b47670bba594f7b71c229df5db3a46d1a979b3
Jakub Narębski <jnareb@gmail.com>
2024-07-11 19:06:04 +0200

    generate_patches.py: Add beginnings of GitRepo class
    This class is based on the GitRepo class in src/utils/git.py in
    https://github.com/ncusi/python_bug_localization (currently private)
    and https://github.com/ncusi/MSR_Challenge_2024

--- src/diffannotator/generate_patches.py
+++ src/diffannotator/generate_patches.py
@@ -19,6 +19,31 @@ import typer
 # TODO: move to __init__.py (it is common to all scripts)
 PathLike = TypeVar("PathLike", str, bytes, Path, os.PathLike)
 
+
+# TODO: extract move to gitrepo.py or utils/git.py
+class GitRepo:
+    """Class representing Git repository, for performing operations on"""
+    path_encoding = 'utf8'
+    default_file_encoding = 'utf8'
+    log_encoding = 'utf8'
+
+    def __init__(self, repo_dir: PathLike):
+        """Constructor for `GitRepo` class
+
+        :param repo_dir: path to the Git repository
+        """
+        # TODO: check that `git_directory` is a path to git repository
+        # TODO: remember absolute path (it is safer)
+        self.repo = Path(repo_dir)
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name}(repo_dir={self.repo!r})"
+
+    def __str__(self):
+        return f"{self.repo!s}"
+
+
 app = typer.Typer(no_args_is_help=True, add_completion=False)
 
 
