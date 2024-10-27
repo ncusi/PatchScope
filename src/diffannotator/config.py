218e1caa8b4e92ba03ad7ece353ef0b80ea01d36
Jakub Narębski <jnareb@mat.umk.pl>
2024-10-27 10:54:33 +0100

    config.py: Prepare for supporting different annotation file formats
    Currently we are planning to support the following formats / schemas:
    - V1, which is the original format from HaPy-Bug paper tool
    - V1_5, which is enhanced format, with reader compatible with V1
    - V2, which is the new format being introduced in this patch series

--- src/diffannotator/config.py
+++ src/diffannotator/config.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
 """Contains configuration for the diffannotator module"""
 import importlib.metadata
+import re
+from enum import Enum
 
 
 __version__: str = "0.1.2"
+
+# configure logging
+logger = logging.getLogger(__name__)
+
+
+class JSONFormat(Enum):
+    V1 = "v1"
+    V1_5 = "v1.5"
+    V2 = "v2"
+
+
+class JSONFormatExt(Enum):
+    V1 = ".json"
+    V1_5 = ".json"
+    V2 = ".v2.json"
+
+
+ext_to_ver: dict[str, JSONFormat] = {
+    str(JSONFormatExt.V1):   JSONFormat.V1,
+    str(JSONFormatExt.V1_5): JSONFormat.V1_5,  # later key "wins"
+    str(JSONFormatExt.V2):   JSONFormat.V2,
+}
+
+secondary_suffixes = { ".v2" }
+secondary_suffix_regexp = re.compile(r"^\.v[0-9]+$")
 
 
 def get_version() -> str:
