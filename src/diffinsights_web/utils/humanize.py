3ccef78750bf21499afd6bffc6d8b07f209274a1
Jakub Narębski <jnareb@mat.umk.pl>
2024-11-25 01:13:11 +0100

    diffinsights_web: Add utils/humanize.py
    This module includes html_date_humane() and html_int_humane(),
    copied from 'diffinsights_web/02-contributors_graph.py'.

---
+++ src/diffinsights_web/utils/humanize.py
@@ -0,0 +1,24 @@
+"""Provide human-readable value, together with machine-readable HTML metadata/microdata"""
+import os
+
+import pandas as pd
+
+
+def html_date_humane(date: pd.Timestamp) -> str:
+    date_format = '%d %a %Y'
+    if os.name == 'nt':
+        date_format = '%#d %a %Y'
+    elif os.name == 'posix':
+        date_format = '%-d %a %Y'
+
+    return f'<time datetime="{date.isoformat()}">{date.strftime(date_format)}</time>'
+
+
+def html_int_humane(val: int) -> str:
+    thousands_sep = " "  # Unicode thin space (breakable in HTML), &thinsp;
+
+    res = f'{val:,}'
+    if thousands_sep != ",":
+        res = res.replace(",", thousands_sep)
+
+    return f'<data value="{val}" style="white-space: nowrap;">{res}</data>'
