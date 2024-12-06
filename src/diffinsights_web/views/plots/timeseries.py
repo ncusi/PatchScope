ncusi/PatchScope

commit 22c44fa80287790b7b3d12088b91a3d52e3b6321

diffinsights_web: Beginnings of mechanism to select plot widget
Now contribution_types_map contains not only the name of the column
to display for plot_commits() (or a special value), but also the
symbolic name for the display widget / pane.  These two pieces of
information are separated by "|" - unlikely to be in column name.

This means that we need to extract plot type from column name in a few
places.  For simplicity, the NO_TEST case does not use the "<plot type>|"
prefix.

To test for correct behavior for unknown, or not supported plots, a
special case of TEST_CASE ("test|TEST") was added.  Few places already
support this.

The value was to be disabled, and is marked as such.  Unfortunately, due
to what looks like bug in Panel, the value is not disabled in HTML (you
can still select it, and it is not grayed out), but there are errors
associated with setting it from UI:

  ERROR: panel.reactive - Callback failed for object named 'Contributions:'
         changing property {'value': 'test|TEST'}
  ValueError: Cannot set the value of Select to 'test|TEST' as it is a disabled option.

src/diffinsights_web/views/plots/timeseries.py
@@ -34,6 +34,15 @@ def plot_commits(resampled_df: pd.DataFrame,
                  xlim: Optional[tuple] = None,
                  ylim: Optional[tuple] = None,
                  kind: str = 'step'):
+    plot_type = "timeline"
+    if '|' in column:
+        plot_type, column = column.split('|', maxsplit=2)
+
+    if plot_type != "timeline":
+        # or maybe even send notification
+        print(f"plot_commits(): expected plot_type of 'timeline', got {plot_type=}")
+        return
+
     # super special case
     if column == SpecialColumnEnum.NO_PLOT.value:
         return
@@ -239,6 +248,13 @@ class TimeseriesPlot(TimelineView):
     def __panel__(self) -> pn.viewable.Viewable:
         if self.column_name == SpecialColumnEnum.NO_PLOT.value:
             return pn.Spacer(height=0)
+
+        if '|' in self.column_name:
+            plot_type, _ = self.column_name.split('|', maxsplit = 1)
+            print(f"TimeseriesPlot.__panel__(): {plot_type=}")
+        else:
+            plot_type = "timeline"
+            print(f"TimeseriesPlot.__panel__(): assuming {plot_type=} for {self.column_name!r}")
 
         return pn.pane.HoloViews(
             self.plot_commits_rx,

src/diffinsights_web/views/__init__.py
@@ -15,20 +15,22 @@ class TimelineView(pn.viewable.Viewer):
 
 
 class SpecialColumnEnum(Enum):
-    LINE_TYPES_PERC = "KIND [%]"
+    LINE_TYPES_PERC = "timeline|KIND [%]"
+    TEST_CASE = "test|TEST"
     NO_PLOT = "<NO PLOT>"
 
 
 #: for the ContributorsHeader.select_contribution_type_widget
 contribution_types_map = {
-    "Commits": "n_commits",
-    "Additions": "+:count",
-    "Deletions": "-:count",
-    "Files changed": "file_names",
-    "Patch size (lines)": "diff.patch_size",
-    "Patch spreading (lines)": "diff.groups_spread",
+    "Commits": "timeline|n_commits",
+    "Additions": "timeline|+:count",
+    "Deletions": "timeline|-:count",
+    "Files changed": "timeline|file_names",
+    "Patch size (lines)": "timeline|diff.patch_size",
+    "Patch spreading (lines)": "timeline|diff.groups_spread",
     # special cases:
     "Line types distribution [%]": SpecialColumnEnum.LINE_TYPES_PERC.value,
+    "(test case, should be disabled)": SpecialColumnEnum.TEST_CASE.value,
     "No plot": SpecialColumnEnum.NO_PLOT.value  # this special value should be last
 }
 column_to_contribution = {

src/diffinsights_web/views/info.py
@@ -93,7 +93,8 @@ class ContributorsHeader(pn.viewable.Viewer):
         self.select_contribution_type_widget = pn.widgets.Select(
             name="Contributions:",
             options=contribution_types_map,
-            value="n_commits",
+            disabled_options=[ SpecialColumnEnum.TEST_CASE.value ],
+            value="timeline|n_commits",  # first value in contribution_types_map
             # style
             width=200,
             margin=(self.widget_top_margin, 0),  # last widget, use x margin of 0
@@ -111,6 +112,14 @@ def sampling_info(resample_freq: str,
                   column: str,
                   frequency_names_map: dict[str, str],
                   min_max_date) -> str:
+    plot_type = "timeline"
+    if '|' in column:
+        plot_type, column = column.split('|', maxsplit=2)
+
+    if plot_type != "timeline":
+        print(f"sampling_info(): expected plot_type of 'timeline', got {plot_type=}")
+        return f"No support for <strong>{plot_type}</strong> plot type, for plotting <em>{column!r}</em>"
+
     contribution_type = column_to_contribution.get(column, "Unknown type of contribution")
 
     return f"""

commit 1d6cc450cbd291e7ebd3586833180108d8782f24

diffinsights_web: Fix LINE_TYPES_PERC case in plot_commits()
Broken since 22c44fa8 or so, because plot_commits() was comparing
"<column>" to "<plot>|<column>".

This commit introduces a bit of code duplication; it does not matter
because the LINE_TYPES_PERC plot will be handled by separate function
in the future, instead of overloading plot_commits().

src/diffinsights_web/views/plots/timeseries.py
@@ -74,7 +74,8 @@ def plot_commits(resampled_df: pd.DataFrame,
             ylim = (-1, ylim[1])
 
     # special cases: y range limits
-    if column == SpecialColumnEnum.LINE_TYPES_PERC.value:
+    if (column == SpecialColumnEnum.LINE_TYPES_PERC.value or
+        f"timeline|{column}" == SpecialColumnEnum.LINE_TYPES_PERC.value):
         ylim = (0.0, 1.05)
 
     # via https://oklch-palette.vercel.app/ and https://htmlcolorcodes.com/rgb-to-hex/
@@ -91,7 +92,8 @@ def plot_commits(resampled_df: pd.DataFrame,
     color = color_map.get(column, '#006dd8')
 
     # special cases: the plot itself
-    if column == SpecialColumnEnum.LINE_TYPES_PERC.value:
+    if (column == SpecialColumnEnum.LINE_TYPES_PERC.value or
+        f"timeline|{column}" == SpecialColumnEnum.LINE_TYPES_PERC.value):
         kind_perc_columns = [
             col for col in resampled_df.columns
             if col.startswith('type.') and col.endswith(' [%]')