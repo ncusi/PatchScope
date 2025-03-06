3af42ea737ba9e9b5ac7d84145512b1af44d927f
Jakub Narębski <jnareb@mat.umk.pl>
2025-03-06 22:40:29 +0100

    benchmarks: Benchmark main components of the 'contributors' app
    This adds the test_contributors_steps_performance() test, in which
    during its creation main steps (main components) of running the
    'contributors' app were benchmarked, and benchmark results were saved
    as comments.
    
    This shows which parts are worth caching, and which do not need it.
    
    While at it, fix the ordering of imports.  All diffinsights_web imports
    must be after import of 'panel' and 'param' via pytest.importorskip()!

--- tests/test_app_contributors_performance.py
+++ tests/test_app_contributors_performance.py
@@ -2,12 +2,13 @@ import io
 
 import pytest
 
-from diffinsights_web.datastore.timeline import get_timeline_data
-
 param = pytest.importorskip("param")
 pn = pytest.importorskip("panel")
 
-from diffinsights_web.apps.contributors import template, dataset_dir, timeline_data_store
+from diffinsights_web.datastore import timeline
+from diffinsights_web.views import info
+from diffinsights_web.views.plots import timeseries
+from diffinsights_web.apps.contributors import template, dataset_dir
 
 
 @pytest.fixture
@@ -67,3 +68,79 @@ def test_contributors_run_performance(app, benchmark):
 
     # Optional: Add an assertion for maximum acceptable render time (in seconds)
     assert result is not None, "App rendering failed"
+
+# add `benchmark` fixture as parameter when performing benchmark (single use)
+def test_contributors_steps_performance():
+    ## TimelineDataStore
+    json_path = dataset_dir.joinpath('qtile.timeline.purpose-to-type.json')
+    # --------------------------------------------------------- benchmark: 1 tests ---------------------------------
+    # (time in ms)        Min       Max      Mean  StdDev    Median      IQR  Outliers     OPS  Rounds  Iterations
+    # --------------------------------------------------------------------------------------------------------------
+    #                100.0428  122.8066  109.2786  7.8773  108.8854  13.8551       4;0  9.1509       9           1
+    # --------------------------------------------------------------------------------------------------------------
+    # timeline_data = benchmark(
+    #     timeline.get_timeline_data,
+    #     json_path=json_path,
+    # )
+    timeline_data = timeline.get_timeline_data(
+        json_path=json_path,
+    )
+    # (time in ms)   105.3941  129.7154  113.6983  8.6321  110.7477  11.2657       1;0  8.7952       7           1
+    timeline_df = timeline.get_timeline_df(
+       timeline_data=timeline_data,
+       repo='qtile',
+    )
+    # (time in us)    62.2000  886.7000   95.1186 53.6248   71.9000  42.7000   240;117  10.5132   2086           1
+    timeline_max_date = timeline.get_max_date(
+        timeline_df=timeline_df,
+    )
+    # (time in us)    34.6000  451.8999   43.8494 20.1701   35.3000  11.5000   532;534  22.8054   6378           1
+    pm_count_cols = timeline.get_pm_count_cols(
+        timeline_df,
+    )
+    # (time in ms)    46.2870   77.5715   55.3805  9.0224   53.0321   9.8213       3;1  18.0569     14           1
+    resampled_timeline_all = timeline.resample_timeline(
+        timeline_df=timeline_df,
+        resample_rate='W',
+        pm_count_cols=pm_count_cols,
+    )
+    # (time in ms)   103.5246  167.7835  129.6302  25.7041  124.3033  38.6812       2;0  7.7143       5           1
+    resampled_timeline_by_author = timeline.resample_timeline(
+        timeline_df=timeline_df,
+        resample_rate='W',
+        group_by='author.email',
+        pm_count_cols=pm_count_cols,
+    )
+
+    ## TimeseriesPlot
+    # (time in us)     1.5999  3,468.3000  2.3201  28.3839  1.8000  0.1001    6;3106      431.0178   14948           1
+    value_range = timeline.get_value_range(
+        timeline_df=resampled_timeline_all,
+        column='n_commits',  # vs 'timeline|n_commits'
+    )
+    date_range = timeline.get_date_range(
+        timeline_df=timeline_df,
+        from_date_str='',
+    )
+    # (time in ms)    57.3498  82.5244  67.2200  8.6832  64.4541  7.6014       2;1  14.8765       6           1
+    authors_info_df = timeline.authors_info_df(
+        timeline_df=timeline_df,
+        column='n_commits',
+        from_date_str='',
+    )
+    # (time in ms)    24.2087  74.8452  44.0435  12.2897  41.6401  15.0689       8;1  22.7048      23           1
+    plot_commits = timeseries.plot_commits(
+        resampled_df=resampled_timeline_all,
+        column='n_commits',
+        from_date_str='',
+    )
+
+    ## RepoPlotHeader
+    #                   Min         Max     Mean   StdDev   Median      IQR  Outliers  OPS (Kops/s)  Rounds  Iterations
+    # (time in us)  22.9999  2,036.5000  45.3154  46.5184  31.7000  30.4000   350;262       22.0675   10061           1
+    sampling_info = info.sampling_info(
+        resample_freq='W',
+        column='n_commits',
+        frequency_names_map=timeline.frequency_names,
+        min_max_date=date_range,
+    )
